c934381ccb3c9e7381269da326df64a9271206da
alejanf2885 <alumno.648000@ies-azarquiel.es>
2025-02-10 21:52:01 +0100

    base updated

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: 'https://alejanf2885.github.io/pokedex'
 })
