4ad4e28094c4834b79cf49bc1cbfa7e932a63902
alejanf2885 <alumno.648000@ies-azarquiel.es>
2025-02-11 09:57:25 +0100

    fix

--- src/components/CardPokemon.tsx
+++ src/components/CardPokemon.tsx
@@ -5,7 +5,7 @@ import { formatPokemonId } from "../utils/pokemonUtils";
 function CardPokemon({ pokemon }: { pokemon: Pokemon }) {
   return (
     <div className="p-2 cursor-pointer rounded-lg transition-all duration-300 bg-gray-200 hover:bg-gray-300 shadow-md 
-                    lg:p-4 xl:p-6 lg:w-80 xl:w-96">
+                    lg:p-4 xl:p-6 lg:w-80 xl:w-96 m-1">
       <div className="flex items-center p-2 lg:p-4 xl:p-6">
         {/* Imagen del Pokémon con tamaños responsivos */}
         <div className="w-16 h-16 sm:w-20 sm:h-20 md:w-24 md:h-24 lg:w-32 lg:h-32 xl:w-40 xl:h-40 flex-shrink-0">
